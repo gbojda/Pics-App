e62ac122b49aa599fafb0322e8901f113911012a
gbbojda <bojdait@gmail.com>
2023-05-29 15:00:19 +0200

    6

--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,5 +1,5 @@
 function ImageShow({ image }) {
-    return <div>{image.id}</div>;
+    return <div> <img src={image.urls.small} alt={image.alt_description} /> </div>;
   }
   
   export default ImageShow;
