f96dd544986666e7c70378fee70d6cc5d3023e64
gbbojda <bojdait@gmail.com>
2023-05-29 13:03:23 +0200

    6

--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,29 +1,6 @@
-import { useState } from 'react';
-
-function SearchBar({ onSubmit }) {
-    const [term, setTerm] = useState('');
-
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-
-       onSubmit(term);
-    };
-
-
-    const handleChange = (event) => {
-        setTerm(event.target.value)
-    };
-
-
-    return ( 
-     <div>
-        <form onSubmit={handleFormSubmit}>
-        <input value={term} onChange={handleChange} />
-        </form>
-     </div>
-    );
-
-   
-}
-
-export default SearchBar;
+function ImageShow({ image }) {
+    return <div>{image.id}</div>;
+  }
+  
+  export default ImageShow;
+  
